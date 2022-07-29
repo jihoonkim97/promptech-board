d7703c134c3361b930bb9347596f1386e56b6ba6
kimjihoon <meaningkim@naver.com>
2022-07-29 14:05:30 +0900

    createPost.html 생성, 컨트롤러에 매핑추가, 페이지 이동 구현

--- src/main/java/co/kr/promptech/controller/BoardController.java
+++ src/main/java/co/kr/promptech/controller/BoardController.java
@@ -7,6 +7,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -22,5 +24,16 @@ public class BoardController {
         model.addAttribute("postList", postService.getPostList());
 
         return "board";
+    }
+
+    @GetMapping("/createPost")
+    public String createPost(){
+        return "createPost";
+    }
+
+    @PostMapping("/createPost")
+    public String createPost(PostVO post){
+        //service
+        return "createPost";
     }
 }
